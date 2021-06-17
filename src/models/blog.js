8a43f4a0b38e1f96a18d273d955772971b5b0fba
Martin Ayush <martinayush04@gmail.com>
2021-06-17 19:32:22 +0530

    Changed File Upload Type

--- src/models/blog.js
+++ src/models/blog.js
@@ -1,43 +1,43 @@
-const mongoose = require('mongoose')
-const moment = require('moment')
+const mongoose = require("mongoose");
+const moment = require("moment");
 
-const blogSchema = mongoose.Schema({
+const blogSchema = mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     category: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     image: {
-        type: Buffer
-    }
-}, {
-    timestamps: true
-})
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-blogSchema.virtual('shortdesc').get(function () {
-    const ans = this.description.slice(0, 100)
-    return ans
-})
+blogSchema.virtual("shortdesc").get(function () {
+  const ans = this.description.slice(0, 100);
+  return ans;
+});
 
-blogSchema.virtual('imagesrc').get(function () {
-    return `data:image/*;base64,${this.image.toString('base64')}`
-})
+blogSchema.virtual("time").get(function () {
+  return moment(this.createdAt).format("MMMM D,YYYY || h:m a");
+});
 
-blogSchema.virtual('time').get(function () {
-    return moment(this.createdAt).format('MMMM D,YYYY || h:m a')
-})
+const Blog = mongoose.model("Blog", blogSchema);
 
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-module.exports = Blog
+module.exports = Blog;
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,82 +1,70 @@
-const express = require('express')
-const router = new express.Router()
-const Blog = require('../models/blog')
-const User = require('../models/User')
-const multer = require('multer')
+const express = require("express");
+const router = new express.Router();
+const Blog = require("../models/blog");
 
-const upload = multer({
-    limits: {
-        fileSize: 1000000
-    }
-})
+router.get("/", async (req, res) => {
+  const blogs = await Blog.find({});
+  res.render("index", {
+    blogs,
+  });
+});
 
+router.get("/blogs/add", (req, res) => {
+  res.render("addBlog");
+});
 
-router.get('/', async (req, res) => {
-    const blogs = await Blog.find({})
-    res.render('index', {
-        blogs
-    })
-})
+router.post("/blogs/add", async (req, res) => {
+  const blog = new Blog({
+    title: req.body.title,
+    description: req.body.description,
+    category: req.body.category,
+    image: req.body.image,
+  });
 
+  try {
+    await blog.save();
+    // res.send("<h1>Blog is Sucessfully saved!</h1>");
+    res.redirect("back");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
-router.get('/blogs/add', (req, res) => {
-    res.render('addBlog')
-})
+router.get("/blogs/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  res.render("fullblog", { blog });
+});
 
-router.post('/blogs/add', upload.single('image'), async (req, res) => {
-    const blog = new Blog({
-        title: req.body.title,
-        description: req.body.description,
-        category: req.body.category,
-        image: req.file.buffer
-    })
+router.get("/blogs/update/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  res.render("updateBlog", {
+    blog,
+  });
+});
 
-    try {
-        await blog.save()
-        // res.send('<h1>Blog is Sucessfully saved!</h1>')
-        res.redirect('back')
-    } catch (error) {
-        res.send(error.message)
-    }
-})
+router.post("/blogs/update/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
 
-router.get('/blogs/:id', async (req, res) => {
-    const blog = await Blog.findById(req.params.id)
-    res.render('fullblog', { blog })
-})
+    (blog.title = req.body.title),
+      (blog.description = req.body.description),
+      (blog.category = req.body.category),
+      (blog.image = req.body.image),
+      await blog.save();
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get('/blogs/update/:id', async (req, res) => {
-    const blog = await Blog.findById(req.params.id)
-    res.render('updateBlog', {
-        blog
-    })
-})
+router.post("/blog/delete/:id", async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.redirect("back");
+    // res.send('<h1>Blog is sucessfully deleted</h1>')
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.post('/blogs/update/:id', upload.single('image'), async (req, res) => {
-    try {
-        const blog = await Blog.findById(req.params.id)
-
-        blog.title = req.body.title
-        blog.description = req.body.description
-        blog.category = req.body.category
-        blog.image = req.file.buffer
-
-        await blog.save()
-        res.redirect('/')
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-router.post('/blog/delete/:id', async (req, res) => {
-    try {
-        await Blog.findByIdAndDelete(req.params.id)
-        res.redirect('back')
-        // res.send('<h1>Blog is sucessfully deleted</h1>')
-    }
-    catch (error) {
-        console.log(error)
-    }
-})
-
-module.exports = router
+module.exports = router;
